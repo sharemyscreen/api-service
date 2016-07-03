8d67a54aa0c71ef2127575d559ecd4f52c48c236
Maxime Guedj <max.guedj13@gmail.com>
2016-07-03 22:35:04 +0200

    Add room information route

--- route/organization/room.js
+++ route/organization/room.js
@@ -4,6 +4,7 @@ const httpHelper = require('sharemyscreen-http-helper');
 
 function registerRoute (router) {
   router.get('/organizations/:id/rooms', passport.authenticate('bearer', { session: false }), getRooms);
+  router.get('/organizations/:orgid/rooms/:roomid', passport.authenticate('bearer', { session: false }), getRoom);
 }
 
 function getRooms (req, res, next) {
@@ -16,6 +17,29 @@ function getRooms (req, res, next) {
       httpHelper.sendReply(res, httpHelper.error.unauthorizedOrganizationDisplay());
     } else {
       httpHelper.sendReply(res, 200, fOrg.safePrint().rooms);
+    }
+  });
+}
+
+function getRoom (req, res, next) {
+  common.organizationModel.getByPublicId(req.params.orgid, function (err, fOrg) {
+    if (err) {
+      next(err);
+    } else if (fOrg == null) {
+      httpHelper.sendReply(res, httpHelper.error.organizationNotFound());
+    } else if (req.user.organizations.indexOf(fOrg._id) === -1) {
+      httpHelper.sendReply(res, httpHelper.error.unauthorizedOrganizationDisplay());
+    } else {
+      var found = false;
+      fOrg.rooms.forEach(function (room) {
+        if (room.publicId === req.params.roomid) {
+          found = true;
+          httpHelper.sendReply(res, 200, room.safePrint());
+        }
+      });
+      if (found === false) {
+        httpHelper.sendReply(res, httpHelper.error.roomNotFound());
+      }
     }
   });
 }
--- test/route/organization/room.js
+++ test/route/organization/room.js
@@ -9,8 +9,9 @@ const apiSrv = supertest(url);
 
 var user1;
 var org;
+var room;
 
-describe('Testing organization routes /v1/organizations/{public-id}/member', function () {
+describe('Testing organization routes /v1/organizations/{public-id}/rooms', function () {
   before(function (done) {
     common.clientModel.createFix(fixture.client.name, fixture.client.key, fixture.client.secret, function (err, cClient) {
       if (err) {
@@ -68,6 +69,7 @@ describe('Testing organization routes /v1/organizations/{public-id}/member', fun
             expect(res.body[0].creator.public_id).to.equal(user1.publicId);
             expect(res.body[0].members).to.have.lengthOf(1);
             expect(res.body[0].members[0].public_id).to.equal(user1.publicId);
+            room = res.body[0];
             done();
           }
         });
@@ -93,6 +95,76 @@ describe('Testing organization routes /v1/organizations/{public-id}/member', fun
     it('Should reply an error if user is not member of the organization', function (done) {
       apiSrv
         .get('/v1/organizations/' + org.publicId + '/rooms')
+        .set('Authorization', 'Bearer ' + fixture.token2)
+        .expect(401)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
+            expect(res.body.code).to.equal(3);
+            expect(res.body.sub_code).to.equal(2);
+            expect(res.body.message).to.equal('Only organization members can perform this action');
+            done();
+          }
+        });
+    });
+  });
+
+  describe('Testing get room information (GET /v1/organization/{public-id}/rooms/{public-id})', function () {
+    it('Should display room information', function (done) {
+      apiSrv
+        .get('/v1/organizations/' + org.publicId + '/rooms/' + room.public_id)
+        .set('Authorization', 'Bearer ' + fixture.token1)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
+            expect(res.body.name).to.equal('general');
+            expect(res.body.creator.public_id).to.equal(user1.publicId);
+            expect(res.body.members).to.have.lengthOf(1);
+            expect(res.body.members[0].public_id).to.equal(user1.publicId);
+            done();
+          }
+        });
+    });
+
+    it('Should reply an error if bad organization id', function (done) {
+      apiSrv
+        .get('/v1/organizations/bad/rooms/' + room.publicId)
+        .set('Authorization', 'Bearer ' + fixture.token1)
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
+            expect(res.body.code).to.equal(3);
+            expect(res.body.sub_code).to.equal(1);
+            expect(res.body.message).to.equal('Unable to find organization');
+            done();
+          }
+        });
+    });
+
+    it('Should reply an error if bad room id', function (done) {
+      apiSrv
+        .get('/v1/organizations/' + org.publicId + '/rooms/bad')
+        .set('Authorization', 'Bearer ' + fixture.token1)
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            done(err);
+          } else {
+            expect(res.body.code).to.equal(4);
+            expect(res.body.sub_code).to.equal(1);
+            expect(res.body.message).to.equal('Unable to find room');
+            done();
+          }
+        });
+    });
+    it('Should reply an error if user is nor member of the organization', function (done) {
+      apiSrv
+        .get('/v1/organizations/' + org.publicId + '/rooms/' + room.publicId)
         .set('Authorization', 'Bearer ' + fixture.token2)
         .expect(401)
         .end(function (err, res) {
